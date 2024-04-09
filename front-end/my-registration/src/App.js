d8b76508cf1c84876cbd7585b4690f9c14cacd15
victor sbityakov <vnsbityakov@gmail.com>
2024-04-09 16:05:13 +0700

    регистрация

---
+++ front-end/my-registration/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import RegistrationPage from './test'; // Предполагая, что ваш компонент находится в файле test.js
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationPage />
+    </div>
+  );
+}
+
+
+export default App;
