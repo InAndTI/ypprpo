54eb9f8fb1e20ba22efc3b7a65d4dc934ba777ad
Lyudmila Ponomareva <l.ponomareva@g.nsu.ru>
2024-04-11 20:49:46 +0700

    Create spring application

---
+++ back-end/peoples_world/src/test/java/nsu/ypprpo/peoples_world/PeoplesWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package nsu.ypprpo.peoples_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplesWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
